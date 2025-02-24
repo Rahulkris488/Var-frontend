ec22d6dd2e173d23a8c6a1f7509560a30af08a44
rahul <rahulkrishnatp12@gmail.com>
2025-02-24 15:55:21 +0530

    Final

--- src/Body.jsx
+++ src/Body.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div className=' mt-20  '>
+    <div className=' mt-10  '>
 
-      <div className='font-extrabold flex items-center justify-center text-6xl '>
+      <div className='font-extrabold flex items-center justify-center text-6xl  '>
         <h1>Want Us to build <span className='transform hover:scale-105 hover:-translate-y-1px from-pink-900 to-blue-900 bg-gradient-to-r text-transparent bg-clip-text transition-all duration-3  00 
                    hover:from-cyan-900 hover:to-green-900 font-serif'>your</span> mini project ?</h1>
       </div>
@@ -12,7 +12,7 @@ const Body = () => {
         <div className='let'>Let us help You with that !</div>
       </div>
       <div className="flex justify-center items-center mt-14  bg-[#a9a59f]">
-  <form className="flex flex-col p-14 w-[350px] border  border-black bg-[#d1cfc8] shadow-[5px_5px_0px_#000]">
+  <form className="flex flex-col mt-8 p-14 w-[350px] border  border-black bg-[#d1cfc8] shadow-[5px_5px_0px_#000]">
     
     <label className="text-black font-semibold mb-1">Name</label>
     <input 
@@ -36,10 +36,10 @@ const Body = () => {
 
   </form>
 </div>
-<div className='flex justify-center items-center p-1 mt-48 text-xl font-mono'>
+<div className='flex justify-center items-center p-1 mt-20 text-xl font-mono'>
 ©2025 Var India,Inc.All Rights Reserved.
 </div>
-    </div>
+</div>
   )
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='justify-between flex items-center ml-36 mr-36 p-10' >
+    <div className='justify-between flex items-center ml-20 mr-20 py-10 ' >
         <h1 className='text-5xl var font-bold transition-all duration-300 transform hover:scale-110'>VAR.</h1>
-        <h2 className='ml-36 font-mono text-2xl transition-all duration-300 transform hover:scale-110 hover:-translate-y-1'>Home</h2>
+        <h2 className='ml-48 font-mono text-2xl transition-all duration-300 transform hover:scale-110 hover:-translate-y-1'>Home</h2>
         <h2 className='font-mono text-2xl transition-all duration-300 transform hover:scale-110 hover:-translate-y-1' >About</h2>
-        <h2 className='font-mono text-2xl transition-all duration-300 transform hover:scale-110 hover:-translate-y-1'>Contact</h2>
+        <h2 className='mr-48 font-mono text-2xl transition-all duration-300 transform hover:scale-110 hover:-translate-y-1'>Contact</h2>
        
-        <button className="relative px-9 py-4 border border-black font-bold text-black bg-transparent
+        <button className="relative px-7 py-3 border border-black font-bold text-black bg-transparent
                    shadow-[4px_4px_0px_black] hover:translate-x-1 hover:translate-y-1 
                    hover:shadow-none transition-all duration-300">
   Sign Up
