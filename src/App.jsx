a94eb6a6a3f84cfcbd9b35d7bbf4584743628a3c
rahul <rahulkrishnatp12@gmail.com>
2025-09-19 19:16:44 +0530

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Header from './Header'
 import Body from './Body'
 
 const App = () => {
   return (
     <div className='min-h-screen w-full  bg-[#a9a59f]'>
-    <Header/>
+
     <Body/>
     </div>
   )
